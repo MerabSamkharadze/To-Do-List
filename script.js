e698e9c94fe4e4162c8bf11c7ee3bef8b81b1014
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-04 21:57:15 +0400

    add remove all button

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ const formElement = document.getElementById("form");
 const inputElement = document.getElementById("input");
 const addButtonElement = document.getElementById("addButton");
 const olEl = document.getElementById("ol");
+const removeAll = document.getElementById("removeAll");
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
   if (inputElement.value === "") return;
   const liEl = document.createElement("li");
-  //   const deleteBtnEl = document.createElement("button");
+
+  const deleteBtnEl = document.createElement("button");
+  deleteBtnEl.innerHTML = ' <i class="fa-solid fa-delete-left"></i>';
+
+  liEl.appendChild(deleteBtnEl);
 
   liEl.textContent = inputElement.value;
   olEl.appendChild(liEl);
   inputElement.value = "";
+});
+
+removeAll.addEventListener("click", () => {
+  olEl.innerHTML = " ";
 });
